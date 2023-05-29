ac3f33d093fab3a45f1993f59728bd732cd367fc
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-05-29 18:08:28 -0400

    game: oneAmongUs

---
+++ backend/functions/src/games/19oneAmongUs.ts
@@ -0,0 +1,26 @@
+import { IGameRules } from "../types/IGameRules";
+
+class oneAmongUs implements IGameRules {
+    players: string[];
+    assignments: { [key: string]: string };
+
+    constructor(players: string[]) {
+        this.players = players;
+        this.assignments = {};
+        this.assignRoles();
+    }
+
+    // Status: ready for testing
+    assignRoles() {
+
+        // assign roles
+        this.assignments[this.players[0]] = 'Target: ' + this.players[3];   // android
+        this.assignments[this.players[1]] = 'Target: ' + this.players[2];   // human leader
+        for (let i = 2; i < this.players.length; i++) {
+            this.assignments[this.players[i]] = "Target: " + this.players[1];   // human
+        }      
+    }
+
+}
+
+export default oneAmongUs;
