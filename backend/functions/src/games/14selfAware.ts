cd333ebe1e746ff8548d633975e83c850d42ab58
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-05-29 17:55:17 -0400

    game: selfAware

---
+++ backend/functions/src/games/14selfAware.ts
@@ -0,0 +1,25 @@
+import { IGameRules } from "../types/IGameRules";
+
+class selfAware implements IGameRules {
+    players: string[];
+    assignments: { [key: string]: string };
+
+    constructor(players: string[]) {
+        this.players = players;
+        this.assignments = {};
+        this.assignRoles();
+    }
+
+    // Status: ready for testing
+    assignRoles() {
+
+        // assign roles
+        this.assignments[this.players[0]] = 'Self-aware android. Human is: ' + this.players[1];
+        for (let i = 1; i < this.players.length; i++) {
+            this.assignments[this.players[i]] = 'Android or Human';
+        }        
+    }
+
+}
+
+export default selfAware;
