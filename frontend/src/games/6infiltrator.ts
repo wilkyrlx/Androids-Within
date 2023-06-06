84534f28c94183dda3d324f03e56976188281372
wilkdave1 <135680920+wilkdave1@users.noreply.github.com>
2023-06-05 22:44:16 -0400

    Updated Infiltrator game text

--- frontend/src/games/6infiltrator.ts
+++ frontend/src/games/6infiltrator.ts
@@ -14,11 +14,11 @@ class infiltrator implements IGameRules {
     assignRoles() {
 
         // assign roles
-        this.assignments[this.players[0]] = "Infiltrator";
-        this.assignments[this.players[1]] = "Android. Your or Following is leader: " + this.players[2];
+        this.assignments[this.players[0]] = "You are the only infiltrator human. Stay concealed and identify the android leader.";
+        this.assignments[this.players[1]] = "You are an android. Your leader must identify the infiltrator. Either you or the following player is the android leader: " + this.players[2];
 
         for (let i = 2; i < this.players.length; i++) {
-            this.assignments[this.players[i]] = "Android. Your or Following is leader: " + this.players[1];
+            this.assignments[this.players[i]] = "You are an android. Your leader must identify the infiltrator. Either you or the following player is the android leader: " + this.players[1];
         }
     }
 
