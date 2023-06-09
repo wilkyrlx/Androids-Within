9ffb7525150d53a8f72dc18d98275660ef555994
wilkdave1 <135680920+wilkdave1@users.noreply.github.com>
2023-06-08 20:45:24 -0400

    Minor text edit to G8

--- frontend/src/games/8chainOfCommand.ts
+++ frontend/src/games/8chainOfCommand.ts
@@ -17,7 +17,7 @@ class chainOfCommand implements IGameRules {
         this.assignments[this.players[0]] = "You are the Alpha Android. Find the Embedded Human. Do not get identified by the Lead Human. The Decoy Android is: " + this.players[1];
         this.assignments[this.players[1]] = "You are the Decoy Android. Find the Embedded Human. Protect the Alpha Android. The Alpha Android is: " + this.players[0];
         this.assignments[this.players[2]] = "You are a human embedded in an android stronghold. Communicate the identity of the Alpha Android to the Lead Human. The Alpha Android is: " + this.players[0];
-        this.assignments[this.players[3]] = "You are the Lead Human. Only you can identify the Alpha Android. The following player is a Human directly below you: " + this.players[4];
+        this.assignments[this.players[3]] = "You are the Lead Human. Only you can call out the Alpha Android. The following player is a Human directly below you: " + this.players[4];
         for (let i = 4; i < this.players.length; i++) {
             let offset = i + 1;
             if (offset == this.players.length) {
