8794eb5ab96e0d3c5fbadcaeaba7c90c86c9c8ff
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-06-11 22:11:41 -0400

    feat: data stored in local storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/global.css';
 import './styles/gamemode.css';
 import './styles/timer.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { pageView } from './types/pageView';
 import Landing from './components/Landing';
 import HostDetails from './components/HostDetails';
@@ -14,27 +14,62 @@ import Timer from './components/Timer';
 import Header from './components/reusable/Header';
 import RoomCode from './components/RoomCode';
 
+const STORAGE_KEY = 'android-game-state';
+
 function App() {
-    
+
     const [room, setRoom] = useState<number>(0);
     const [playerID, setPlayerID] = useState<number>(-1);	// playerID is the index of the player in the array of players in the backend
-	const [isHost, setHost] = useState<boolean>(false);	    // is the player the host of the game? [true/false]
+    const [isHost, setHost] = useState<boolean>(false);	    // is the player the host of the game? [true/false]
     const [view, setView] = useState<pageView>(pageView.LANDING)	// which page is being displayed
     const [playerCount, setPlayerCount] = useState<number>(0);	// number of players in the game [0-12]
     const [timerDuration, setDuration] = useState<number>(0);	// timer length (minutes)
 
+    const [isInitialMount, setIsInitialMount] = useState(true);
+
+    useEffect(() => {
+        if (!isInitialMount) {
+            // Save the state to localStorage
+            const state = {
+                room,
+                playerID,
+                isHost,
+                view,
+                playerCount,
+                timerDuration,
+            };
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+        } else {
+            setIsInitialMount(false);
+        }
+    }, [room, playerID, isHost, view, playerCount, timerDuration]);
+
+    useEffect(() => {
+        // Retrieve the state from localStorage on component mount
+        const savedState = localStorage.getItem(STORAGE_KEY);
+        if (savedState) {
+            const state = JSON.parse(savedState);
+            setRoom(state.room);
+            setPlayerID(state.playerID);
+            setHost(state.isHost);
+            setView(state.view);
+            setPlayerCount(state.playerCount);
+            setDuration(state.timerDuration);
+        }
+    }, []);
+
     return (
         <div id="App" >
             <Header />
-            { view === pageView.LANDING && <Landing setView={setView} /> }
-            { view === pageView.HOST_DETAILS && <HostDetails setView={setView} setRoom={setRoom} setPlayerID={setPlayerID} setHost={setHost} setPlayerCount={setPlayerCount} /> }
-            { view === pageView.JOIN_DETAILS && <JoinDetails setView={setView} setRoom={setRoom} setPlayerID={setPlayerID} /> }
-            { view === pageView.ROOM_CODE && <RoomCode setView={setView} room={room} /> }
-            { view === pageView.GAMEMODE_SELECT && <GamemodeSelect setView={setView} room={room} playerCount={playerCount} setDuration={setDuration} /> }
-            { view === pageView.WAITING && <Waiting setView={setView} room={room} /> }
-            { view === pageView.PLAYING && <Playing setView={setView} room={room} playerID={playerID} isHost={isHost} timerDuration={timerDuration}/> }
-            { view === pageView.TIMER && <Timer setView={setView} timerDuration={timerDuration} /> }
-            { view === pageView.ALL_ROLES && <AllRoles setView={setView} room={room} /> }
+            {view === pageView.LANDING && <Landing setView={setView} />}
+            {view === pageView.HOST_DETAILS && <HostDetails setView={setView} setRoom={setRoom} setPlayerID={setPlayerID} setHost={setHost} setPlayerCount={setPlayerCount} />}
+            {view === pageView.JOIN_DETAILS && <JoinDetails setView={setView} setRoom={setRoom} setPlayerID={setPlayerID} />}
+            {view === pageView.ROOM_CODE && <RoomCode setView={setView} room={room} />}
+            {view === pageView.GAMEMODE_SELECT && <GamemodeSelect setView={setView} room={room} playerCount={playerCount} setDuration={setDuration} />}
+            {view === pageView.WAITING && <Waiting setView={setView} room={room} />}
+            {view === pageView.PLAYING && <Playing setView={setView} room={room} playerID={playerID} isHost={isHost} timerDuration={timerDuration} />}
+            {view === pageView.TIMER && <Timer setView={setView} timerDuration={timerDuration} />}
+            {view === pageView.ALL_ROLES && <AllRoles setView={setView} room={room} />}
         </div>
     );
 }
--- frontend/src/components/reusable/Header.tsx
+++ frontend/src/components/reusable/Header.tsx
@@ -1,8 +1,13 @@
+function clearData() {
+    localStorage.clear();
+    window.location.reload();
+}
 
 function Header() {
     return (
         <div id="header">
             <h1>Androids Within</h1>
+            <div onClick={() => clearData()}>Reset</div>
         </div>
     )
 }
